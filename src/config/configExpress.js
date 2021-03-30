777ac95a4129facf9dffee20c1358455978e30ac
Welisson luca <welissonluca17@gmail.com>
2021-03-30 20:14:07 -0300

    fix: server running erros

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const consign = require("consign");
-const bodyPaser = require("body-paser");
+
 
 module.exports = () => {
     const app = express();
     app.use(express.json());
-    consign()
-        .include('controllers')
-        .into(app)
+    consign({ cwd: "src" })
+        .include("controllers")
+        .into(app);
     
 
     return app;
